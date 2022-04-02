87cd81f887d18181f4def12594b36b897c8602dd
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-04-02 11:54:55 +0530

    argument parsing with yargs

--- app.js
+++ app.js
@@ -12,8 +12,20 @@ yargs.version('1.1.0');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function(){
-    console.log("Adding a new note");
+  builder:{
+    title: {
+      describe: 'Note Title',
+      demandOption: true,
+      type: 'string'
+    }, 
+    body: {
+      describe: 'Note Body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    console.log("Title: ", argv.title, argv.body);
   }
 })
 
@@ -44,5 +56,6 @@ yargs.command({
   }
 })
 
+yargs.parse();
 
-console.log(yargs.argv);
+// console.log(yargs.argv);
