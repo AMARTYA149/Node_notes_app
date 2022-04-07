416fbfbe26152b34bed9cd0f9abeba8547ccb008
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-04-07 22:46:52 +0530

    add notes

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
 // const fs = require('fs');
 
 // fs.appendFileSync('notes.txt', 'This file created by Amartya!!');
-const chalk = require('chalk');
-const yargs = require('yargs');
-const getNotes = require('./notes');
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
 
 //customise yargs version
-yargs.version('1.1.0');
+yargs.version("1.1.0");
 
 // create add command
 yargs.command({
-  command: 'add',
-  describe: 'Add a new note',
-  builder:{
+  command: "add",
+  describe: "Add a new note",
+  builder: {
     title: {
-      describe: 'Note Title',
+      describe: "Note Title",
       demandOption: true,
-      type: 'string'
-    }, 
+      type: "string",
+    },
     body: {
-      describe: 'Note Body',
+      describe: "Note Body",
       demandOption: true,
-      type: 'string'
-    }
+      type: "string",
+    },
   },
-  handler: function(argv){
-    console.log("Title: ", argv.title, argv.body);
-  }
-})
+  handler: function (argv) {
+    notes.addNotes(argv.title, argv.body);
+  },
+});
 
 // create remove command
 yargs.command({
-  command: 'remove',
-  describe: 'Remove a new note',
-  handler: function(){
+  command: "remove",
+  describe: "Remove a new note",
+  handler: function () {
     console.log("Removing a note");
-  }
-})
+  },
+});
 
 // create list command
 yargs.command({
-  command: 'list',
-  describe: 'List notes',
-  handler: function(){
+  command: "list",
+  describe: "List notes",
+  handler: function () {
     console.log("Listing note");
-  }
-})
+  },
+});
 
 // create add command
 yargs.command({
-  command: 'read',
-  describe: 'Read a note',
-  handler: function(){
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
     console.log("Reading a note");
-  }
-})
+  },
+});
 
 yargs.parse();
 
