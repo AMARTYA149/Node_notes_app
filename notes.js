e725e4754370c2528c9c4f0ef3d25ca3b70d52e6
AMARTYA149 <amartya970914@gmail.com>
2022-05-23 23:36:33 +0530

    read notes

--- notes.js
+++ notes.js
@@ -7,11 +7,12 @@ const getNotes = () => {
 
 const addNotes = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
+
   // const duplicateNotes = notes.filter(function (note) {
   //   return note.title === title;
   // });
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({ title: title, body: body });
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added!"));
@@ -59,9 +60,22 @@ const loadNotes = () => {
   }
 };
 
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((item) => item.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
 module.exports = {
   getNotes: getNotes,
   addNotes: addNotes,
   removeNote: removeNote,
   listNotes: listNotes,
+  readNote: readNote,
 };
